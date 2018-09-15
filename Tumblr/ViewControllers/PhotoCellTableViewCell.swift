a052eb50d073d321347a2c3d8964d9708d7e74bd
abraham asmile <babyyuguioh@gmail.com>
2018-09-15 17:12:07 -0400

    project good

---
+++ Tumblr/ViewControllers/PhotoCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCellTableViewCell.swift
+//  Tumblr
+//
+//  Created by Mac on 6/24/1397 AP.
+//  Copyright © 1397 Abraham Asmile. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Tumblr/ViewControllers/PhotosViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PhotosViewController.swift
+//  Tumblr
+//
+//  Created by Mac on 6/24/1397 AP.
+//  Copyright © 1397 Abraham Asmile. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
